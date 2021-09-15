e9c441615f2aeeb0bbf8be3cf4db529309a32b3d
unknown <shrutijangid5601@gmail.com>
2021-09-15 18:03:48 +0530

    added bootstrap popper.js and jquery

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import './css/LandingPage.css';
 import './components/config/fire.js'
 import LandingPage from './components/LandingPage';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+import Homepage from './components/Homepage';
 function App() {
   return (
     <>
+
     <Router>
-    <LandingPage/>
+
+    <Switch>
+{/* route for landing page */}
+      <Route exact path="/">
+        <LandingPage/>
+      </Route>
+{/* route for homepage */}
+      <Route path="/app">
+        <Homepage/>
+      </Route>
+
+    </Switch>
+
     </Router>
+
     </>
   );
 }
